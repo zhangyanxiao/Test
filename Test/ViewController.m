8a11d3c518ca7c73408b0974b265d85f6ed13638
zhangyanxiao <385584895@qq.com>
2016-09-02 17:16:18 +0800

    第一次提交
    第一次提交项目

---
+++ Test/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by 公安信息 on 16/9/2.
+//  Copyright © 2016年 zhangyanxiao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor redColor];
+    
+    
+    UIView * view1 = [[UIView alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
+    view1.backgroundColor = [UIColor yellowColor];
+    [self.view addSubview:view1];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
